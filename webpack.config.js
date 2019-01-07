0869275587005afcc7a82c35051a16cf3c157d23
SauSap <tuan.back@classfunc.com>
2019-01-08 00:31:15 +0700

    fix

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: './AdvancedGrid.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+        modules: [
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+};
