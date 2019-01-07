9ce2a8d6b06f348c74d0760e01c67d30ebadce49
SauSap <tuan.back@classfunc.com>
2019-01-07 22:32:07 +0700

    Up component AdvancedGrid.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grids from './AdvancedGrid'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {/*<p>*/}
-            {/*Edit <code>src/App.js</code> and save to reload.*/}
-          {/*</p>*/}
-          <a
-            className="App-link"
-            href="https://classfunc.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            About the Classfunc company
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Grids container item xs={24} justify={'center'}>
+                        <img src={logo} className="App-logo" alt="Classfunc"/>
+                    </Grids>
+                    {/*<p>*/}
+                        {/*Edit <code>src/App.js</code> and save to reload.*/}
+                    {/*</p>*/}
+                    <Grids container item xs={24} justify={'center'}>
+                        <a
+                            className="App-link"
+                            href="https://classfunc.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            About the Classfunc
+                        </a>
+                    </Grids>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
